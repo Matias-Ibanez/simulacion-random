29a36f43321cc51e69f06e390787fa8800e0ee07
Matias-Ibanez <matiasagustinibanez@outlook.com>
2025-04-11 02:17:15 -0300

    cambio de vista

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 from ui.second_frame import  MethodFrame
 from methods.cuadrado import _ParteCentralDelCuadrado
 from methods.lehmer import _Lehmer
@@ -26,34 +27,40 @@ class MainApp(ctk.CTk):
 
         self._set_appearance_mode("Dark")
         self.title("Generador de números aleatorios")
-        self.width= 600
-        self.height= 400
+        self.width= 800
+        self.height= 600
         self.center_window()
         self.resizable(False, False)
 
         self.menu_frame = ctk.CTkFrame(self,fg_color="transparent")
         self.menu_frame.pack(fill="both", expand=True, padx=20, pady=20)
+
+        self.frame = ctk.CTkFrame(master=self.menu_frame, fg_color = "#292840", width=450,
+                                  height=450, border_width=3, border_color="#8449BF" )
+        self.frame.pack(expand = True)
+        self.frame.pack_propagate(False)
         self._load_menu()
 
         self.main_container = ctk.CTkFrame(self)
         self.main_container.pack_forget()
 
     def _load_menu(self):
+
         self.label = ctk.CTkLabel(
-            master=self.menu_frame,
+            master=self.frame,
             text="Seleccionar método",
             font=("Inter", 20, "bold"),
             fg_color="transparent").pack(pady=30)
 
         for method in self.method_configs.keys():
-             ctk.CTkButton(master=self.menu_frame,
+             ctk.CTkButton(master=self.frame,
                            text=f"{method}",
-                           width=220,
+                           width=280,
                            corner_radius=32,
-                           font=("Inter", 14, "bold"),
-                           fg_color="#10B981",
+                           font=("Inter", 18, "bold"),
+                           fg_color="#8449BF",
                            hover_color="#059669",
-                           command=lambda m=method: self.open_method_frame(m)).pack(pady=10)
+                           command=lambda m=method: self.open_method_frame(m)).pack(pady=18)
 
     def center_window(self):
         window_width = self.winfo_screenwidth()
@@ -76,3 +83,5 @@ class MainApp(ctk.CTk):
     def show_main_menu(self):
         self.main_container.pack_forget()
         self.menu_frame.pack(fill="both", expand=True)
+
+
--- ui/second_frame.py
+++ ui/second_frame.py
@@ -1,5 +1,8 @@
 import customtkinter as ctk
 
+def only_numbers(valor):
+    return valor.isdigit() or valor == ""
+
 class MethodFrame(ctk.CTkFrame):
     def __init__(self, master, method, config, back_callback):
         super().__init__(master)
@@ -10,18 +13,18 @@ class MethodFrame(ctk.CTkFrame):
         self._load_menu()
 
     def _load_menu(self):
-
-        self.input_frame = ctk.CTkFrame(master=self, corner_radius=15)
+        self.input_frame = ctk.CTkFrame(master=self, corner_radius=15, fg_color = "#292840",
+                                        width=450, border_width=3, border_color="#8449BF")
         self.input_frame.pack(side= "left", fill="y", padx=10, pady=10)
 
-        self.result_frame = ctk.CTkFrame(master=self, corner_radius=15, height=150)
+        self.result_frame = ctk.CTkFrame(master=self, corner_radius=15, height=300, fg_color = "#059669",)
         self.result_frame.pack(side= "top", fill= "x", padx=10, pady=10)
         self.result_frame.pack_propagate(False)
 
         self.test_frame = ctk.CTkFrame(master=self, corner_radius=15)
-        self.test_frame.pack(side= "bottom", fill="x", padx=10, pady=10)
+        self.test_frame.pack(fill="both", padx=10, pady=10)
 
-        j_title = ctk.CTkLabel(master=self.result_frame, text = self.method_name)
+        j_title = ctk.CTkLabel(master=self.result_frame, text = self.method_name, font=("Inter", 25, "bold"))
         j_title.pack(side= "top")
 
         self.results_scroll = ctk.CTkScrollableFrame(master=self.result_frame,
@@ -30,24 +33,79 @@ class MethodFrame(ctk.CTkFrame):
                                                      )
         self.results_scroll.pack(side= "top",fill="x", padx=10, pady=10)
 
+        validate_numbers = self.register(only_numbers)
+
         for label_text in self.config["labels"]:
-            label = ctk.CTkLabel(self.input_frame, text=label_text)
-            label.pack(pady=(10, 0))
-            entry = ctk.CTkEntry(self.input_frame, placeholder_text=label_text)
-            entry.pack(pady=(0, 5))
+
+            label = ctk.CTkLabel(
+                self.input_frame,
+                text=label_text,
+                font=("Inter", 18, "bold")
+            )
+            label.pack(fill="x", padx=20, pady=(10, 10))
+
+            entry = ctk.CTkEntry(
+                self.input_frame,
+                placeholder_text="Solo números",
+                border_color="#059669",
+                width=180,
+                validate="key",
+                validatecommand=(validate_numbers, "%P")
+            )
+            entry.pack(fill = "x", padx = 20,  pady=(0, 5))
             self.entries.append(entry)
 
-        ctk.CTkButton(self.input_frame, text="Generar",command=self._call_method).pack(padx=10 ,pady=15)
+        ctk.CTkButton(
+            self.input_frame,
+            text="Generar",
+            command=self._call_method,
+            fg_color="#8449BF",
+            hover_color="#059669",
+            font=("Inter", 18, "bold")
+        ).pack(padx=10, pady=15)
 
-        ctk.CTkButton(self.input_frame, text="← Volver", command=self.back_callback).pack(pady=10)
-
+        ctk.CTkButton(
+            self.input_frame,
+            text="← Volver",
+            command=self.back_callback,
+            fg_color="#8449BF",
+            hover_color="#059669",
+            font=("Inter", 18, "bold")
+        ).pack(pady=10)
 
     def _call_method(self):
-        method = self.config["method"]
-        args = [int(entry.get()) for entry in self.entries]
-        result = method(*args)
-        for index,result in enumerate(result):
-            ctk.CTkLabel(self.results_scroll, text=f"U{index+1} = {result}").pack(pady=10)
+        for widget in self.results_scroll.winfo_children():
+            widget.destroy()
+        try:
+            method = self.config["method"]
+            args = [int(entry.get()) for entry in self.entries]
+            result = method(*args)
+
+            for index, value in enumerate(result):
+                ctk.CTkLabel(
+                    self.results_scroll,
+                    text=f"U{index + 1} = {value}",
+                    font=("Inter", 18)
+                ).pack(pady=10)
+
+
+        except ValueError:
+            self.error_label = ctk.CTkLabel(
+                self,
+                text="¡Error! Campos vacíos",
+                font=("Inter", 15),
+            )
+            self.error_label.pack(pady=10)
+            self.after(2000, self.error_label.destroy)
+
+
+
+
+
+
+
+
+
 
 
 
